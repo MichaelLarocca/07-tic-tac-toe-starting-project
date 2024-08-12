8fdc9f560beb2aac2c79ec08bdfab98b12ce84ed
Larocca <Larocca@RIVPRO1.net>
2024-08-12 18:49:32 -0400

    Working on handleEditClick

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,13 +4,13 @@ export default function Player({name, symbol}) {
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
-        setIsEditing(true)
+        setIsEditing(!isEditing);
     }
 
     let playerName = <span className="player-name">{name}</span>;
 
     if(isEditing) {
-        playerName = <input type="text" required/>
+        playerName = <input type="text" required value={name} />
     }
 
     return (
@@ -23,7 +23,7 @@ export default function Player({name, symbol}) {
                 {isEditing && <input></input>} */}
             </span>
             {/* <button onClick={()=>{setIsEditing(true)}}>Edit</button> */}
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     );
 }
