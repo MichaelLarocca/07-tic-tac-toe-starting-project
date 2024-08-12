3ecdf866fe9db0167887a01ededf2fb8c34ff7ef
Larocca <Larocca@RIVPRO1.net>
2024-08-12 18:36:24 -0400

    Added an if statement to check the useState variable

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
+
 export default function Player({name, symbol}) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing(true)
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+
+    if(isEditing) {
+        playerName = <input type="text" required/>
+    }
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
-                <span className="player-symbol">{symbol}</span>
+                {playerName}
+                {/* {!isEditing && <span className="player-name">{name}</span>}
+                {isEditing && <input></input>}
+                {!isEditing && <span className="player-symbol">{symbol}</span>}
+                {isEditing && <input></input>} */}
             </span>
-            <button>Edit</button>
+            {/* <button onClick={()=>{setIsEditing(true)}}>Edit</button> */}
+            <button onClick={handleEditClick}>Edit</button>
         </li>
     );
 }
