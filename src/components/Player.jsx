ec7cccf6d82ce1d43049f1d6485e044f33979916
Larocca <Larocca@RIVPRO1.net>
2024-08-13 16:15:03 -0400

    Added name change feature

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,29 +1,31 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
-        setIsEditing(!isEditing);
+        setIsEditing((isEditing) => !isEditing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name" >{playerName}</span>;
 
     if(isEditing) {
-        playerName = <input type="text" required value={name} />
+        editablePlayerName = (
+            <input type="text" required value={playerName} onChange={handleChange} />
+        )    
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}
-                {/* {!isEditing && <span className="player-name">{name}</span>}
-                {isEditing && <input></input>}
-                {!isEditing && <span className="player-symbol">{symbol}</span>}
-                {isEditing && <input></input>} */}
+                {editablePlayerName}
             </span>
-            {/* <button onClick={()=>{setIsEditing(true)}}>Edit</button> */}
-            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+                <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     );
 }
