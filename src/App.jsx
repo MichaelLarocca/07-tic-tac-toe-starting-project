f2e5f5020c31b10309ae6dbce1726fef5d70f622
Larocca <Larocca@RIVPRO1.net>
2024-08-20 18:32:54 -0400

    Display winning name

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,10 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+  const [players, setPlayers]= useState({
+    'X': 'Player 1',
+    'O': 'Player 2'
+  });
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = deriveActivePlayer(gameTurns);
 
@@ -45,7 +49,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol && 
       firstSquareSymbol === thirdSquareSymbol) {
 
-      winner = firstSquareSymbol;  
+      winner = players[firstSquareSymbol];  
     }
   }
 
@@ -64,16 +68,25 @@ function App() {
     });
   }
 
-function handleRestart() {
-    setGameTurns([]);
-  }
+  function handleRestart() {
+      setGameTurns([]);
+    }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      };
+    });
+  }  
 
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName={"Player 1"} symbol={"X"} isActive={activePlayer === "X"} />
-          <Player initialName={"Player 2"} symbol={"O"} isActive={activePlayer === "O"} />
+          <Player initialName={"Player 1"} symbol={"X"} isActive={activePlayer === "X"} onChangeName={handlePlayerNameChange} />
+          <Player initialName={"Player 2"} symbol={"O"} isActive={activePlayer === "O"} onChangeName={handlePlayerNameChange} />
         </ol>
         {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
         <GameBoard 
